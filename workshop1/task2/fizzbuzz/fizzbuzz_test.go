b153d3e072fd7fad9735e90bd86b1df71af15f01
ilya.bogdanov <ilya.bogdanov@ispringsolutions.com>
2018-12-01 12:00:06 +0300

    Added benchmark to fizzbuzz tests

--- workshop1/task2/fizzbuzz/fizzbuzz_test.go
+++ workshop1/task2/fizzbuzz/fizzbuzz_test.go
@@ -15,10 +15,14 @@ func TestFizzBuzz(t *testing.T) {
 		{15, "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz"},
 		{36, "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, Fizz Buzz, 31, 32, Fizz, 34, Buzz, Fizz"},
 	}
-	for _, c := range (cases) {
+	for _, c := range cases {
 		result := FizzBuzz(c.count)
-		if (result != c.expected) {
+		if result != c.expected {
 			t.Errorf("Unexpected result: expected %q, got %q", c.expected, result)
 		}
 	}
+}
+
+func BenchmarkFizzBuzz(b *testing.B) {
+	FizzBuzz(b.N)
 }
