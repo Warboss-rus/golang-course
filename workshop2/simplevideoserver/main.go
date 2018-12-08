66ced7e94459c8377a8434e9e7305186a9871d5a
ilya.bogdanov <ilya.bogdanov@ispringsolutions.com>
2018-12-08 11:32:52 +0300

    Extracted handlers to its own package, refactor them so each method is handled in its own file

--- workshop2/simplevideoserver/main.go
+++ workshop2/simplevideoserver/main.go
@@ -1,27 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"github.com/Warboss-rus/golang-course/workshop2/simplevideoserver/handlers"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/api/v1/list", func(w http.ResponseWriter, _ *http.Request) {
-		fmt.Fprint(w, `[{
-			"id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-			"name": "Black Retrospetive Woman",
-			"duration": 15,
-			"thumbnail": "/content/d290f1ee-6c54-4b01-90e6-d701748f0851/screen.jpg"
-		}]`)
-	})
-	http.HandleFunc("/api/v1/video/d290f1ee-6c54-4b01-90e6-d701748f0851", func(w http.ResponseWriter, _ *http.Request) {
-		fmt.Fprint(w, `{
-	    	"id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-    		"name": "Black Retrospetive Woman",
-    		"duration": 15,
-    		"thumbnail":"/content/d290f1ee-6c54-4b01-90e6-d701748f0851/screen.jpg",
-    		"url":"/content/d290f1ee-6c54-4b01-90e6-d701748f0851/index.mp4" 
-		}`)
-	})
-	http.ListenAndServe(":8000", nil)
+	router := handlers.Router()
+	_ = http.ListenAndServe(":8000", router)
 }
