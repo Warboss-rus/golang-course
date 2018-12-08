30b48acd8f977d98b3c95340740f9536381c608c
ilya.bogdanov <ilya.bogdanov@ispringsolutions.com>
2018-12-08 12:18:31 +0300

    Added graceful shutdown

--- workshop2/simplevideoserver/main.go
+++ workshop2/simplevideoserver/main.go
@@ -1,11 +1,40 @@
 package main
 
 import (
+	"context"
 	"github.com/Warboss-rus/golang-course/workshop2/simplevideoserver/handlers"
 	log "github.com/sirupsen/logrus"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 )
+
+func startServer(serverUrl string) *http.Server {
+	router := handlers.Router()
+	srv := &http.Server{Addr: serverUrl, Handler: router}
+	go func() {
+		log.Fatal(srv.ListenAndServe())
+	}()
+
+	return srv
+}
+
+func getKillSignalChan() chan os.Signal {
+	osKillSignalChan := make(chan os.Signal, 1)
+	signal.Notify(osKillSignalChan, os.Interrupt, syscall.SIGTERM)
+	return osKillSignalChan
+}
+
+func waitForKillSignal(killSignalChan <-chan os.Signal) {
+	killSignal := <-killSignalChan
+	switch killSignal {
+	case os.Interrupt:
+		log.Info("got SIGINT...")
+	case syscall.SIGTERM:
+		log.Info("got SIGTERM...")
+	}
+}
 
 func main() {
 	log.SetFormatter(&log.JSONFormatter{})
@@ -17,6 +46,10 @@ func main() {
 
 	const serverUrl = ":8000"
 	log.WithFields(log.Fields{"url": serverUrl}).Info("starting the server")
-	router := handlers.Router()
-	log.Fatal(http.ListenAndServe(serverUrl, router))
+
+	killSignalChan := getKillSignalChan()
+	srv := startServer(serverUrl)
+
+	waitForKillSignal(killSignalChan)
+	_ = srv.Shutdown(context.Background())
 }
